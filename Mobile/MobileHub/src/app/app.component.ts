3f44a400d054c9fe86790c017b6bdf732e751018
aniketnaik321 <aniket.naik321@gmail.com>
2024-06-02 16:27:53 +0530

    changes
    changes

--- Mobile/MobileHub/src/app/app.component.ts
+++ Mobile/MobileHub/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import {
-  ActionPerformed,
-  PushNotificationSchema,
-  PushNotifications,
-  Token,
-} from '@capacitor/push-notifications';
+import { Component, OnInit } from '@angular/core';
+// import {  
+//   PushNotification,
+//   PushNotificationToken,
+//   PushNotificationActionPerformed,
+// } from '@capacitor/core';
+
+//const { PushNotifications } = Plugins;
+
 
 @Component({
   selector: 'app-root',
@@ -12,50 +14,47 @@ import {
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
-
   ngOnInit() {
     console.log('Initializing HomePage');
 
     // Request permission to use push notifications
     // iOS will prompt user and return if they granted permission or not
     // Android will just grant without prompting
-    PushNotifications.requestPermissions().then(result => {
-      if (result.receive === 'granted') {
-        // Register with Apple / Google to receive push via APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Show some error
-      }
-    });
+    // PushNotifications.requestPermission().then( result => {
+    //   if (result.granted) {
+    //     // Register with Apple / Google to receive push via APNS/FCM
+    //     PushNotifications.register();
+    //   } else {
+    //     // Show some error
+    //   }
+    // });
 
+    // // On success, we should be able to receive notifications
+    // PushNotifications.addListener('registration',
+    //   (token: PushNotificationToken) => {
+    //     alert('Push registration success, token: ' + token.value);
+    //   }
+    // );
 
-   // On success, we should be able to receive notifications
-   PushNotifications.addListener('registration',
-   (token: Token) => {
-     alert('Push registration success, token: ' + token.value);
-   }
- );
+    // // Some issue with our setup and push will not work
+    // PushNotifications.addListener('registrationError',
+    //   (error: any) => {
+    //     alert('Error on registration: ' + JSON.stringify(error));
+    //   }
+    // );
 
- // Some issue with our setup and push will not work
- PushNotifications.addListener('registrationError',
-   (error: any) => {
-     alert('Error on registration: ' + JSON.stringify(error));
-   }
- );
+    // // Show us the notification payload if the app is open on our device
+    // PushNotifications.addListener('pushNotificationReceived',
+    //   (notification: PushNotification) => {
+    //     alert('Push received: ' + JSON.stringify(notification));
+    //   }
+    // );
 
- // Show us the notification payload if the app is open on our device
- PushNotifications.addListener('pushNotificationReceived',
-   (notification: PushNotificationSchema) => {
-     alert('Push received: ' + JSON.stringify(notification));
-   }
- );
-
- // Method called when tapping on a notification
- PushNotifications.addListener('pushNotificationActionPerformed',
-   (notification: ActionPerformed) => {
-     alert('Push action performed: ' + JSON.stringify(notification));
-   }
- );
-  }
+    // // Method called when tapping on a notification
+    // PushNotifications.addListener('pushNotificationActionPerformed',
+    //   (notification: PushNotificationActionPerformed) => {
+    //     alert('Push action performed: ' + JSON.stringify(notification));
+    //   }
+    // );
+}
 }
